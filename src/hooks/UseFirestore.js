3d8208adbd5ba1dfb7f0fd3de24f40310f913163
Planting12 <simej1214@gmail.com>
2023-06-09 23:45:29 +0900

    2023.06.09 실습

---
+++ src/hooks/UseFirestore.js
@@ -0,0 +1,64 @@
+import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
+import { useReducer } from "react";
+import { appFireStore, Timestamp } from "../firebase/config";
+
+const initState = {
+  document: null,
+  isPending: false,
+  error: null,
+  isSuccess: false,
+};
+
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case "isPending":
+      return { isPending: true, document: null, isSuccess: false, error: null };
+    case "addDoc":
+      return {
+        isPending: false,
+        document: action.payload,
+        isSuccess: true,
+        error: null,
+      };
+    case "error":
+      return {
+        isPending: true,
+        document: null,
+        isSuccess: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useFirestore = (transaction) => {
+  const [response, dispatch] = useReducer(storeReducer, initState);
+
+  const colRef = collection(appFireStore, transaction);
+  console.log(colRef);
+
+  const addDocument = async (doc) => {
+    dispatch({ type: "isPending" });
+    try {
+      const createTime = Timestamp.fromDate(new Date());
+      const docRef = await addDoc(colRef, { ...doc, createTime });
+      console.log(docRef);
+      dispatch({ type: "addDoc", payload: docRef });
+    } catch (error) {
+      dispatch({ type: "error", payload: error.message });
+    }
+  };
+
+  const deleteDocument = async (id) => {
+    dispatch({ type: "pending" });
+    try {
+      const docRef = await deleteDoc(doc(colRef, id));
+      dispatch({ type: "deleteDoc", payload: docRef });
+    } catch (error) {
+      dispatch({ type: "error", payload: error.message });
+    }
+  };
+
+  return { addDocument, deleteDocument, response };
+};
